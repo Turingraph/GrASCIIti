32fb6750c1107c7f0098ca932f0efa287621f505
Turingraph <nemocheche@gmail.com>
2026-05-31 14:15:00 +0700

    Implement f_floor

--- src/libft/arr.c
+++ src/libft/arr.c
@@ -39,3 +39,22 @@ void	free_nest_arr(void **arr, size_t len)
 	}
 	free(arr);
 }
+
+// https://stackoverflow.com/questions/41856771/
+// write-your-own-implementation-of-maths-floor-function-c
+
+// time : O(1)
+// space: O(1)
+double	f_floor(double num)
+{
+    long long	n;
+    double		d;
+
+	n = (long long)num;
+	d = (double)n;
+	if (num >= 2147483647 || num <= -2147483648 || num != num)
+        return (num);
+    if (d == num || num >= 0)
+        return (d);
+    return (d - 1);
+}
