5a09debaeadf00ad2834f51afd6b8562bddb5bbf
Turingraph <nemocheche@gmail.com>
2026-08-12 12:51:22 +0700

    Simplify coding examples

--- coding_examples/editor/line/straight_line.c
+++ coding_examples/editor/line/straight_line.c
@@ -4,25 +4,20 @@ int	main(int len, char **str)
 {
 	t_table_fdf	table;
 	int			output;
-	t_line		line;
-	t_line		boundary;
-	t_ink		ink;
+	t_line		line = {
+		.p1 = {.x = 15, .y = 90},
+		.p2 = {.x = 65, .y = 35}
+	};
+	t_line		boundary = {
+		.p1 = {.x = 25, .y = 0},
+		.p2 = {.x = 75, .y = 101}
+	};
+	t_ink		ink = {.channel = HEIGHT, .color = 10, .thickness = 12};
 
-	boundary.p1.x = 25;
-	boundary.p1.y = 0;
-	boundary.p2.x = 75;
-	boundary.p2.y = 101;
-	ink.channel = HEIGHT;
-	ink.color = 10;
 	if (len < 2)
 		return (0);
 	output = open_dir_file(str[1], NULL, APPEND);
 	table = init_table_fdf(101, 101, false);
-	ink.thickness = 12;
-	line.p1.x = 15;
-	line.p1.y = 90;
-	line.p2.x = 65;
-	line.p2.y = 35;
 	draw_straight_line(&table, line, boundary, ink);
 	line.p2.x = 15;
 	draw_straight_line(&table, line, boundary, ink);
