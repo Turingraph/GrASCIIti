d22b1863b4ba816e3df9a7ef350adbb7bfc681ea
Phuri Sottatipreedawong <phsottat@z2t3c1.42bangkok.com>
2026-08-14 19:15:02 +0700

    There is only one BEER and 6 days Left.

---
+++ src/graphic/view/table.c
@@ -0,0 +1,15 @@
+#include"view.h"
+
+// time : O(n)
+// space: O(n)
+int	view_table_fdf(const t_table_fdf *src)
+{
+	mlx_image_t	dst;
+
+	if (src == NULL || src->arr == NULL
+		|| src->row == 0 || src->col == 0)
+		return (-1);
+	dst = mlx_new_image()
+	return (1);
+}
+
---
+++ src/graphic/view/view.h
@@ -0,0 +1,7 @@
+#ifndef VIEW_H
+# define VIEW_H
+
+#include "../../utils/type/type.h"
+#include "MLX42.h"
+
+#endif
