60d28e6efbb05eb918862e46bb770058935a7b34
Turingraph <nemocheche@gmail.com>
2026-08-22 18:05:00 +0700

    Refactor view_master_piece

--- src/graphic/window/view.c
+++ src/graphic/window/view.c
@@ -17,6 +17,33 @@ mlx_image_t	*handle_mlx_error(mlx_t *mlx, size_t width, size_t height)
 	return (dst);
 }
 
+// time : O(1)
+// space: O(1)
+t_2d_hook	init_2d_hook(mlx_t *mlx, t_motif_arr *motif,
+	t_ink32 drawing_style, t_background background)
+{
+	t_2d_hook	dst;
+
+	dst.mlx = mlx;
+	dst.camera = NULL;
+	dst.master_piece.still_life = NULL;
+	dst.master_piece.motif = motif;
+	dst.master_piece.background = background;
+	dst.master_piece.drawing_style = drawing_style;
+	if (mlx != NULL)
+		dst.master_piece.tiles = init_tile_format(
+			mlx->width, mlx->height,
+			(size_t)f_interval(background.tiles_resolution, 0, 10));
+	else
+		dst.master_piece.tiles = init_tile_format(
+			1, 1,
+			(size_t)f_interval(background.tiles_resolution, 0, 10));
+	dst.img = NULL;
+	if (mlx != NULL)
+		dst.img = handle_mlx_error(mlx, mlx->width, mlx->height);
+	return (dst);
+}
+
 // 1920 * (3 / 4) = 1440
 // 1080 * (3 / 4) = 810
 // time : O(n)
@@ -25,35 +52,30 @@ void	view_master_piece(t_motif_arr *motif,
 	t_ink32 drawing_style, t_background background)
 {
 	mlx_t			*mlx;
-	mlx_image_t		*img;
 	t_2d_hook		hook;
 	t_2d_camera		camera;
 
-	camera = init_2d_camera(1440, 810);
 	mlx = mlx_init(1440, 810, "Subset at 4:42pm", true);
-	img = handle_mlx_error(mlx, 1440, 810);
-	hook.img = img;
-	if (img == NULL || hook.img == NULL)
+	hook = init_2d_hook(mlx, motif, drawing_style, background);
+	if (hook.img == NULL)
+	{
+		if (mlx != NULL)
+			mlx_terminate(mlx);
 		return ;
-	color_background_mlx(img, background.color);
-	hook.mlx = mlx;
+	}
+	color_background_mlx(hook.img, background.color);
+	camera = init_2d_camera(1440, 810);
 	hook.camera = &camera;
-	hook.master_piece.still_life = NULL;
-	hook.master_piece.motif = motif;
-	hook.master_piece.tiles = init_tile_format(
-		1440, 810, (size_t)f_interval(background.tiles_resolution, 0, 10));
-	hook.master_piece.drawing_style = drawing_style;
-	hook.master_piece.background = background;
-	// draw_motif_mlx(&hook, true);
-	if (-1 == mlx_image_to_window(mlx, img,
-		(mlx->width - img->width) / 2,
-		(mlx->height - img->height) / 2))
+	draw_motif_mlx(&hook, true);
+	if (-1 == mlx_image_to_window(mlx, hook.img,
+		(mlx->width - hook.img->width) / 2,
+		(mlx->height - hook.img->height) / 2))
 	{
 		mlx_terminate(mlx);
-		return  ;
+		return ;
 	}
-	// mlx_key_hook(mlx, &hook_pan_motif, &hook);
+	mlx_key_hook(mlx, &hook_pan_motif, &hook);
 	mlx_loop(mlx);
-	mlx_delete_image(mlx, img);
+	mlx_delete_image(mlx, hook.img);
 	mlx_terminate(mlx);
 }
